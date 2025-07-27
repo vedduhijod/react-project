c79128e6ae722a059061d21560f45386c60e628a
Ved Duhijod <vedduhijod@gmail.com>
2025-07-27 21:07:10 +0530

    add service for database and storage

---
+++ 11blogapp/src/conf/conf.js
@@ -0,0 +1,7 @@
+const conf = {
+  appwriteUrl: String(process.env.VITE_APPWRITE_APPWRITE_URL),
+  appwriteProjectId: String(procvess.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(process.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(process.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(process.env.VITE_APPWRITE_BUCKET_ID),
+};
